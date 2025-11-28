2c3722d0446377624619a43e90928fa26976b6c3
Rohit-hub616 <ruhilrohit9@gmail.com>
2025-11-28 19:46:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "21216779"
 api_hash = "fdad88238287459c6dc790658f1008c1"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+bot_token = "8215483820:AAGMd0a76s1_6d0W56WIx2gPCJzakJd319s"
+auth_users = [728093729]
+sudo_users = [728093729]
