4dac51fbc29e324bb4c70b7743c3a1cd0d3efbd6
Rohit-hub616 <ruhilrohit9@gmail.com>
2025-11-28 19:43:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
+api_id = "21216779"
+api_hash = "fdad88238287459c6dc790658f1008c1"
 bot_token = "bot token"
 auth_users = [7890781002]
 sudo_users = [7890781002]
